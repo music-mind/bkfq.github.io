c1b956149d5e9576f2c85784baddc0eae3b95558
Kevin zhu <kszhu@edu.uwaterloo.ca>
2017-03-11 21:49:58 -0500

    detects thumbs up/down

--- test.js
+++ test.js
@@ -5,11 +5,12 @@ Leap.loop(function(frame) {
   frame.hands.forEach(function(hand, index) {
     
     var cat = ( cats[index] || (cats[index] = new Cat()) );    
-    cat.setTransform(hand.screenPosition(), hand.roll());
+    cat.setTransform(hand.screenPosition(), hand.roll(), hand.grabStrength);
     
   });
   
 }).use('screenPosition', {scale: 0.25});
+
 
 
 var Cat = function() {
@@ -22,8 +23,25 @@ var Cat = function() {
     document.body.appendChild(img);
   }
   
-  cat.setTransform = function(position, rotation) {
+  cat.setTransform = function(position, rotation, grip) {
 	
+	document.getElementById("demo").innerHTML = rotation;
+
+if (grip == 1) {
+	if (rotation > -1.8 && rotation < -1.3) {
+		document.getElementById("message").innerHTML = "Hold your thumbs up!";
+	}
+	else if (rotation > 1.1 && rotation < 1.6) {
+		document.getElementById("message").innerHTML = "Hold your thumbs down!";
+	}
+	else {
+		document.getElementById("message").innerHTML = "Thumbs up or Thumbs down!";
+	}
+}
+	else {
+		document.getElementById("message").innerHTML = "Thumbs up or Thumbs down!";
+	}
+
     img.style.left = position[0] - img.width  / 2 + 'px';
     img.style.top  = position[1] - img.height / 2 + 'px';
 
@@ -35,6 +53,8 @@ var Cat = function() {
   };
 
 };
+
+
 
 cats[0] = new Cat();
 
