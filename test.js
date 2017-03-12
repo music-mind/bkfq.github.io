9a33b541b8c038eecc52f3c1c32d44697d109dbf
alexbie98 <alexbie98@gmail.com>
2017-03-12 03:44:56 -0400

    hide weird message when not plugged in

--- test.js
+++ test.js
@@ -1,16 +1,18 @@
 var cats = {};
 
 
-function show(){
-	document.getElementById("count").style.visibility="visible";
-	//document.getElementById("demo").style.visibility="visible";
-	img.style.visibility = "visible";
-	
+function show() {
+    document.getElementById("count").style.visibility = "visible";
+    //document.getElementById("demo").style.visibility="visible";
+    img.style.visibility = "visible";
+
 }
 
+document.getElementById("message").style.visibility="hidden";
 
 
-var lastPos = [0,0,0];
+
+var lastPos = [0, 0, 0];
 var currentPos = [];
 
 var counter = 0;
@@ -21,59 +23,60 @@ var hand;
 
 Leap.loop(function(frame) {
 
-  if (started){
-    frame.hands.forEach(function(hand, index) {
-    
-	if (index>0){
-	    return;
-	}
-	var cat = ( cats[index] || (cats[index] = new Cat()) );    
-	cat.setTransform(hand.screenPosition(), hand.roll(), hand.grabStrength);
-    
-    });
-  }
 
-  
-  else{
 
-	if (document.getElementById("message").innerHTML != "Show open palm to start!"){
-	document.getElementById("message").innerHTML="Show open palm to start!";
-	img.style.visibility="hidden";
-	console.log("flipped");
-}
-	hand = frame.hands[0];
-    
-	if (!started && hand){  
-	    // check if hand is open
-	    if (hand.grabStrength> 0){
-	    counter = 0;
-	    }
+    if (started) {
+        frame.hands.forEach(function(hand, index) {
+
+            if (index > 0) {
+                return;
+            }
+            var cat = (cats[index] || (cats[index] = new Cat()));
+            cat.setTransform(hand.screenPosition(), hand.roll(), hand.grabStrength);
+
+        });
+    } else {
 	
-	
-	    currentPos = hand.palmPosition;
+	document.getElementById("message").style.visibility="visible";	
 
-	    for (var i=0; i< 3;i++){
-		if (Math.abs(currentPos[i]-lastPos[i])>=2){
-		    counter = 0;
-		}
-	    }
+        if (document.getElementById("message").innerHTML != "Show open palm to start!") {
+            document.getElementById("message").innerHTML = "Show open palm to start!";
+        }
+        hand = frame.hands[0];
 
-	    lastPos = currentPos;
+        if (!started && hand) {
+            // check if hand is open
+            if (hand.grabStrength > 0) {
+                counter = 0;
+            }
 
-	    if (counter>=30){
-		startPos = currentPos;
-		started = true;
-		
-		reset();
-		show();
-	    }
 
-	    counter++;
-	}
-  }
+            currentPos = hand.palmPosition;
 
-  
-}).use('screenPosition', {scale: 0.25});
+            for (var i = 0; i < 3; i++) {
+                if (Math.abs(currentPos[i] - lastPos[i]) >= 2) {
+                    counter = 0;
+                }
+            }
+
+            lastPos = currentPos;
+
+            if (counter >= 30) {
+                startPos = currentPos;
+                started = true;
+
+                reset();
+                show();
+            }
+
+            counter++;
+        }
+    }
+
+
+}).use('screenPosition', {
+    scale: 0.25
+});
 
 
 
@@ -87,119 +90,119 @@ var check4 = false;
 var img;
 
 var Cat = function() {
-  var cat = this;
-  img = document.createElement('img');
-  img.src = 'down.png';
-  img.style.position = 'absolute';
-  img.onload = function () {
-    cat.setTransform([window.innerWidth/2,window.innerHeight/2], 0);
-    document.body.appendChild(img);
-  }
-  
-  cat.setTransform = function(position, rotation, grip) {
-	
-	//document.getElementById("demo").innerHTML = rotation;
+    var cat = this;
+    img = document.createElement('img');
+    img.src = 'down.png';
+    img.style.position = 'absolute';
+    img.onload = function() {
+        cat.setTransform([window.innerWidth / 2, window.innerHeight / 2], 0);
+        document.body.appendChild(img);
+    }
 
-if (grip == 1) {
-	if (rotation > -1.8 && rotation < -1.3) {
-		document.getElementById("message").innerHTML = "Keep your thumbs up!";
-		if (!x) {
-			if (event) {
-			clearTimeout(myVar);
-			}
-			event = true;
-			myVar = setTimeout(myFunction, 1000);
-			
-			x = true;
-		}
-	}
-	else if (rotation > 1.1 && rotation < 1.6) {
-		document.getElementById("message").innerHTML = "Keep your thumbs down!";
-		if (!x) {
-			if (event) {
-			clearTimeout(myVar);
-			}
-			event = true;
-			myVar = setTimeout(myFunction, 1000);
-			x = true;
-		}
-	}
-	else {
-		document.getElementById("message").innerHTML = "Thumbs up or Thumbs down!";
-		if (x) {
-			x = false;
-			document.getElementById("count").innerHTML = "Please hold still.";
-		}
-	}
-}
-	else {
-		document.getElementById("message").innerHTML = "Thumbs up or Thumbs down!";
-		if (x) {
-			x = false;
-			document.getElementById("count").innerHTML = "Please hold still.";
-		}
-	}
+    cat.setTransform = function(position, rotation, grip) {
 
-    img.style.left = position[0] - img.width  / 2 + 'px';
-    img.style.top  = position[1] - img.height / 2 + 'px';
+        //document.getElementById("demo").innerHTML = rotation;
 
-	img.style.transform = 'rotate(' + -rotation + 'rad)';
-	
-    img.style.webkitTransform = img.style.MozTransform = img.style.msTransform =
-    img.style.OTransform = img.style.transform;
+        if (grip == 1) {
+            if (rotation > -1.8 && rotation < -1.3) {
+                document.getElementById("message").innerHTML = "Keep your thumbs up!";
+                if (!x) {
+                    if (event) {
+                        clearTimeout(myVar);
+                    }
+                    event = true;
+                    myVar = setTimeout(myFunction, 1000);
 
-  };
+                    x = true;
+                }
+            } else if (rotation > 1.1 && rotation < 1.6) {
+                document.getElementById("message").innerHTML = "Keep your thumbs down!";
+                if (!x) {
+                    if (event) {
+                        clearTimeout(myVar);
+                    }
+                    event = true;
+                    myVar = setTimeout(myFunction, 1000);
+                    x = true;
+                }
+            } else {
+                document.getElementById("message").innerHTML = "Thumbs up or Thumbs down!";
+                if (x) {
+                    x = false;
+                    document.getElementById("count").innerHTML = "Please hold still.";
+                }
+            }
+        } else {
+            document.getElementById("message").innerHTML = "Thumbs up or Thumbs down!";
+            if (x) {
+                x = false;
+                document.getElementById("count").innerHTML = "Please hold still.";
+            }
+        }
+
+        img.style.left = position[0] - img.width / 2 + 'px';
+        img.style.top = position[1] - img.height / 2 + 'px';
+
+        img.style.transform = 'rotate(' + -rotation + 'rad)';
+
+        img.style.webkitTransform = img.style.MozTransform = img.style.msTransform =
+            img.style.OTransform = img.style.transform;
+
+    };
+
+	img.style.visibility = "hidden";
+
 
 };
 
 
 
+
+
+
+
 function myFunction() {
-	if (x) {
-    		document.getElementById("count").innerHTML = "3";
-		myVar = setTimeout(myFunction2, 1000);
-	}
-	else {
-		document.getElementById("count").innerHTML = "Please hold still.";
-	}
+    if (x) {
+        document.getElementById("count").innerHTML = "3";
+        myVar = setTimeout(myFunction2, 1000);
+    } else {
+        document.getElementById("count").innerHTML = "Please hold still.";
+    }
 }
 
 function myFunction2() {
-	if (x) {
-    		document.getElementById("count").innerHTML = "2";
-		myVar = setTimeout(myFunction3, 1000);
-	}
-	else {
-		document.getElementById("count").innerHTML = "Please hold still.";
-	}
+    if (x) {
+        document.getElementById("count").innerHTML = "2";
+        myVar = setTimeout(myFunction3, 1000);
+    } else {
+        document.getElementById("count").innerHTML = "Please hold still.";
+    }
 }
 
 function myFunction3() {
-	if (x) {
-    		document.getElementById("count").innerHTML = "1";
-		myVar = setTimeout(myFunction4, 1000);
-	}
-	else {
-		document.getElementById("count").innerHTML = "Please hold still.";
-	}
+    if (x) {
+        document.getElementById("count").innerHTML = "1";
+        myVar = setTimeout(myFunction4, 1000);
+    } else {
+        document.getElementById("count").innerHTML = "Please hold still.";
+    }
 }
 
 function myFunction4() {
-	if (x) {
-    		document.getElementById("count").innerHTML = "Congratulations!";
-		document.getElementById("next").style.visibility = 'visible';
-	}
-	else {
-		document.getElementById("count").innerHTML = "Please hold still.";
-	}
+    if (x) {
+        document.getElementById("count").innerHTML = "Congratulations!";
+        document.getElementById("next").style.visibility = 'visible';
+    } else {
+        document.getElementById("count").innerHTML = "Please hold still.";
+    }
 }
 
 
 function reset() {
-	document.getElementById("message").innerHTML = "Thumbs up or Thumbs down!";
-	document.getElementById("count").innerHTML = "Please hold still.";
-	document.getElementById("next").style.visibility="hidden";
-	x = false;
+    document.getElementById("message").innerHTML = "Thumbs up or Thumbs down!";
+    document.getElementById("count").innerHTML = "Please hold still.";
+    document.getElementById("next").style.visibility = "hidden";
+    x = false;
 }
 
 
@@ -207,5 +210,7 @@ function reset() {
 
 cats[0] = new Cat();
 
+
+
 // This allows us to move the cat even whilst in an iFrame.
-Leap.loopController.setBackground(true)
+//Leap.loopController.setBackground(true)
